564da572c98c9f490a272e98cd44e8090dab91f3
Sidin <Sidin@DESKTOP-HC6QCIF>
2021-08-21 13:23:56 -0700

    atividade do dia 21/08/2021

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,6 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+
+Route::resource('animal', AnimalController::class);
