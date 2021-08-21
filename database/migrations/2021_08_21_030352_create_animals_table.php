6b041ee8f46e94a3522f5246c28b039759dd69f0
Sidin <Sidin@DESKTOP-HC6QCIF>
2021-08-21 12:32:49 -0700

    por favir que der certo

---
+++ database/migrations/2021_08_21_030352_create_animals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnimalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->id(); 
+           $table codigo
+           $table data de nascimento
+           $table sexo
+           $table peso
+           $table data da pesagem
+           $table observacoes
+           $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+}
